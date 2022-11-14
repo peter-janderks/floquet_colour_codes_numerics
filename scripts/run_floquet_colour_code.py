2c21f68094fa3cae2794f81b09ad13f09e68dea9
Peter Johannes Derks <derksp97@physik.fu-berlin.de>
2022-11-14 10:13:00 +0100

    first run from desktop

--- scripts/run_floquet_colour_code.py
+++ scripts/run_floquet_colour_code.py
@@ -207,6 +207,7 @@ def load_or_create_stim_circuit(
 if __name__ == '__main__':
 #    biases = [64, 128]
     # distances = [4, 8, 12, 16]
+    """
     biases= [32,64,128]
     distances = [8, 12, 16,20]
 #    ps = [0.0013,0.0014,0.0015,0.0016]
@@ -215,10 +216,11 @@ if __name__ == '__main__':
     max_n_errors = 1000
     main('FloquetColourCode',ps,biases,distances, max_n_shots, max_n_errors)   
 #    main('Honeycomb',ps,biases,distances, max_n_shots, max_n_errors)   
-
+    """
+    
     biases= [2,4,8,16]
     distances = [8, 12, 16,20]
-    ps = np.linspace(0.003,0.006,10)#, dtype='float8
+    ps = np.linspace(0.003,0.006,30)#, dtype='float8
     max_n_shots = 100000
     max_n_errors = 1000
     main('FloquetColourCode',ps,biases,distances, max_n_shots, max_n_errors)   
